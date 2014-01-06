ca00fb067565b2b4203ec372da111e4eb18b90e5
Leo Zhang <leo@lehaozhang.com>
2014-01-06 00:57:31 -0800

    Integrated sockets example.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var http = require('http')
 // Initialise and return the server.
 module.exports = function(callback){
   var server = http.createServer(app);
+
+  // Want to use Sockets.IO? You could do it this way:
+  // var initialise_sockets = require('./lib/sockets');
+  // initialise_sockets(server);
+
   server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
     callback(server);
